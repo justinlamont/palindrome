b66f4b831297b8b1c0c35957eb531ca7eb00bfd0
Justin <lamontjl@gmail.com>
2020-09-01 19:05:19 -0400

    fixed typo and added readme

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ function Phrase(content) {
     // Returns content processed for palindrome testing.
     this.processedContent = function processedContent() {
         
-        eturn this.content.toLowerCase();
+        return this.content.toLowerCase();
     }
 
     // Returns true if the phrase is a palindrome, false otherwise.
